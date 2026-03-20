e043ea5b93a0d2bc3e9f4f5f975fec36473fffa4
RamzyChatti90 <ramzychatti90@gmail.com>
2026-03-20 23:49:58 +0100

    [YC-PROJET-005] UPDATE_FILE: src/main/webapp/app/layouts/main/main.component.ts

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -1,40 +1,22 @@
-import { Component, OnInit, Renderer2, RendererFactory2, inject } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
-import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
-import dayjs from 'dayjs/esm';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
-import { AccountService } from 'app/core/auth/account.service';
-import { AppPageTitleStrategy } from 'app/app-page-title-strategy';
-import FooterComponent from '../footer/footer.component';
-import PageRibbonComponent from '../profiles/page-ribbon.component';
+import { JhiNavbarComponent } from '../navbar/navbar.component';
+import { JhiFooterComponent } from '../footer/footer.component';
+import { JhiPageRibbonComponent } from '../../shared/page-ribbon/page-ribbon.component';
+import { CarnivalWidgetComponent } from '../../shared/carnival-widget/carnival-widget.component';
 
 @Component({
   selector: 'jhi-main',
+  standalone: true,
   templateUrl: './main.component.html',
-  providers: [AppPageTitleStrategy],
-  imports: [RouterOutlet, FooterComponent, PageRibbonComponent],
+  imports: [CommonModule, RouterOutlet, JhiNavbarComponent, JhiFooterComponent, JhiPageRibbonComponent, CarnivalWidgetComponent],
 })
-export default class MainComponent implements OnInit {
-  private readonly renderer: Renderer2;
-
-  private readonly router = inject(Router);
-  private readonly appPageTitleStrategy = inject(AppPageTitleStrategy);
-  private readonly accountService = inject(AccountService);
-  private readonly translateService = inject(TranslateService);
-  private readonly rootRenderer = inject(RendererFactory2);
-
-  constructor() {
-    this.renderer = this.rootRenderer.createRenderer(document.querySelector('html'), null);
-  }
+export class MainComponent implements OnInit {
+  constructor() {}
 
   ngOnInit(): void {
-    // try to log in automatically
-    this.accountService.identity().subscribe();
-
-    this.translateService.onLangChange.subscribe((langChangeEvent: LangChangeEvent) => {
-      this.appPageTitleStrategy.updateTitle(this.router.routerState.snapshot);
-      dayjs.locale(langChangeEvent.lang);
-      this.renderer.setAttribute(document.querySelector('html'), 'lang', langChangeEvent.lang);
-    });
+    // Logic to initialize component
   }
 }
